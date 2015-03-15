25bcf43a5c3c7a211d551cfdc7978497db6bd87c
KamalWadhwa007 <kamal_wadhwa@yahoo.com>
2015-03-16 01:39:54 +0530

    fixed the bat_pack bugs+ UT test code files, also removed unnecessary files

--- bat-sim_final.cpp
+++ bat-sim_final.cpp
@@ -1,22 +1,41 @@
 #include<iostream>
-#include<string.h>
+#include<string>
 #include"3bat_pack.h"
+#include<stdio.h>
+#include<iostream>
+#include<cstdlib>
+#include<pthread.h>
 
+
+Bat_pack batPack;
 
 using namespace std;
 
-char **convert_command_to_words(char *)
+void *stop_wait(void *x)
 {
+Bat_pack *w = static_cast<Bat_pack*>(x);
 
-
+	while(w->sim_is_ON())
+	{
+		if('.' == getchar())
+		{
+		w->stop_sim();
+		getchar();
+		break;
+		}	
+	}
+pthread_exit(NULL);
 }
+
 
 int main()
 {
 
-Bat_pack batPack;
+
 float temp_f;
 string command;
+//string::size_type sz;
+
 
 
 while(command != "exit")
@@ -25,26 +44,29 @@ while(command != "exit")
 
 	cin >> command;
 	
-	if(!strcmp(command, "set"))	//starts with set
+	if((command == "set"))	//starts with set
 	{
-		if(batPack.sim_is_ON())
+		/*if(batPack.sim_is_ON())
 		{
 			cout << "ERROR :: stop simulation first to modify any value \n";
 
 			continue;
-		}
+		}*/
 		
 		cin >> command;
-		if(!strcmp(command, "initial")) //second arg is initail	
+		if((command == "initial")) //second arg is initail	
 		{
 			cin >> command;
-			if(!strcmp(command, "voltage:")) // 3rd arg is 'voltage:'
+			if((command == "voltage:")) // 3rd arg is 'voltage:'
 			{
 			
 			for(int k=0; k < _N; k++) // 4th arg to _N are voltage values in float
 			{
 			cin >> command;
-			temp_f =stof(command);
+
+/*GNU compiler issue with < 4.8 versions with stof and strof, not working directly on 'string' type, using const char* instead*/
+			//temp_f =strtof(command,&sz);
+			sscanf(command.c_str(), "%f",&temp_f);
 			batPack.bat[k].set_vol(temp_f);
 
 			}
@@ -56,21 +78,23 @@ while(command != "exit")
 				continue;
 			 }
 
-		}else if(!strcmp(command, "load:")) //second arg 'load:'
+		}else if((command == "load:")) //second arg 'load:'
 		{
 
 			cin >> command; //3rd arg is then R_load in ohms
-			temp_f =stof(command);
+			//temp_f = strtof(command,&sz);
+			sscanf(command.c_str(), "%f",&temp_f);
 			batPack.set_R_Load(temp_f);
 
 			continue;			
 
-		}else if(!strcmp(command, "series_Rs:")) //second arg 'series_Rs'
+		}else if((command == "series_Rs:")) //second arg 'series_Rs'
 		{
 			for(int k=0; k < _N; k++) // 3th arg to _N are series resistance in float
 			{
 			cin >> command;
-			temp_f =stof(command);
+			// temp_f =strtof(command,&sz);
+			sscanf(command.c_str(), "%f",&temp_f);
 		batPack.bat[k].set_ser_res(temp_f);							
 			}
 			continue;			
@@ -82,14 +106,15 @@ while(command != "exit")
 		}
 
 	}//all set commands completed above	
-	else if(!strcmp(command, "get"))
+	else if((command == "get"))
 	{
 		cin >> command;
 
-		if(!strcmp(command, "switch")) //second arg is initail	
+		if((command == "switch")) //second arg is initail	
 		{
 			cin >> command;
-			if(!strcmp(command, "state")) // 3rd arg is 'voltage:'
+
+			if((command == "state")) // 3rd arg is 'voltage:'
 			{
 			 for(int k=0; k < _N; k++) 
 			{
@@ -103,7 +128,7 @@ while(command != "exit")
 			}
 		continue;
 		}
-		else if(!strcmp(command, "voltage")) //second arg is initail	
+		else if((command == "voltage")) //second arg is initail	
 		{
 			for(int k=0; k < _N; k++) 
 			{
@@ -111,11 +136,11 @@ while(command != "exit")
 			}
 		continue;		
 		}
-		else if(!strcmp(command, "current")) //second arg is initail
+		else if((command == "current")) //second arg is initail
 		{
 			for(int k=0; k < _N; k++) // 3th arg to _N are series resistance in float
 			{
-				cout << "\nB" << (k+1) << " current: " << batPack.bat[k].get_vol()<< endl;							
+				cout << "\nB" << (k+1) << " current: " << batPack.bat[k].get_i_load()<< endl;							
 			}
 
 		continue;
@@ -126,21 +151,26 @@ while(command != "exit")
 		}
 
 	}// all get commands handled
-	else if(!strcmp(command, "start"))
+	else if((command == "start"))
 	{
 		cin >> command;
-		if(!strcmp(command, "simulation"))
-		{batPack.run_sim();}
+		if(command == "simulation")
+		{	
+			pthread_t thread_to_stop;
+			pthread_create(&thread_to_stop, NULL, stop_wait, &batPack);
+			batPack.run_sim();
+
+		}
 		else
 		{	cout << "ERROR using 'start' command, expected -- 'start simulation'\n";}
 
 		continue;
 		
 	}
-	else if(!strcmp(command, "stop"))
+	else if((command == "stop"))
 	{
 		cin >> command;
-		if(!strcmp(command, "simulation"))
+		if((command == "simulation"))
 		{batPack.stop_sim();}
 		else
 		{	cout << "ERROR using 'stop' command, expected -- 'stop simulation'\n";}
@@ -148,13 +178,14 @@ while(command != "exit")
 		continue;
 		
 	}
+	else if(command != "exit") {
+		cout << "INVALID Command please refer Readme file\n";
+	}
 
-
-cout << "INVALID Command please refer Readme file\n";
 
 
 }
-
+pthread_exit(NULL);
 
 
 }
